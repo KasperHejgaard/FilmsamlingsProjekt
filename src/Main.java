aefae64c2122d97a3b350bf314dcaa5a37c512ed
KasperHejgaard <kahe0003@stud.kea.dk>
2024-02-20 14:52:19 +0100

    Filmsamling dag 1.

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        MovieCollection nyliste = new MovieCollection();
+        //titel
+        System.out.println("Hvad er titlen på filem");
+        String title = input.nextLine();
+
+        //director
+        System.out.println("Hvem er director på filmen?");
+        String director = input.nextLine();
+
+        //yearCreated
+        System.out.println("Hvilket år udkom filmen?");
+        int yearCreated = input.nextInt();
+
+        //isInColor
+        System.out.println("Er filmen på farver?");
+        boolean isInColor = input.nextBoolean();
+
+        //lengthInMinutes
+        System.out.println("Hvor lang er filmen i minutter?");
+        int lengthInMinutes = input.nextInt();
+
+        //Genre
+        System.out.println("Hvilken genre er filmen?");
+        String genre = input.next();
+
+        nyliste.addMovie(title, director, yearCreated, isInColor, lengthInMinutes, genre);
+
+        Movie[] movieList = nyliste.getMovieList();
+        System.out.println(movieList[0].getTitle() + " er blevet tilføjet til listen");
+
+    }
+}
---
+++ src/MovieCollection.java
@@ -0,0 +1,18 @@
+public class MovieCollection {
+
+    private Movie[] movieList;
+    int count = 0;
+
+    public MovieCollection() {
+        this.movieList = new Movie[5];
+    }
+
+    public void addMovie(String title, String director, int yearCreated, boolean isInColor, int lengthInMinutes, String genre) {
+        movieList[count++] = new Movie(title, director, yearCreated, isInColor, lengthInMinutes, genre);
+    }
+
+    public Movie[] getMovieList() {
+        return movieList;
+    }
+
+}
